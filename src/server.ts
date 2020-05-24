1f6846609cdc8e8079b394bf9dde985021cee22b
sj <sascha.jaschke@gmail.com>
2020-05-24 20:35:10 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const app = express();
 
     //CORS Should be restricted
     app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", c.url);
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         next();
     });
